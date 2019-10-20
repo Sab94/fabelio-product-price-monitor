3b9b2104f070268f73294fc04f5e993e2c2205f2
Sab94 <asabya@gmail.com>
2019-10-20 11:20:09 +0530

    wirking on ng

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from '.components/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
